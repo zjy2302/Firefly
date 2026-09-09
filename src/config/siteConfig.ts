ef746f534c33ea53a645a6267001f49d805236a9
zjy2302 <15732733901@163.com>
2026-09-09 06:29:06 +0000

    更新个人信息

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -52,11 +52,11 @@ export const siteConfig: SiteConfig = {
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"这是兔兔的个人博客……",
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
+		"Zjy",
 		"Fuwari",
 		"Astro",
 		"ACGN",
